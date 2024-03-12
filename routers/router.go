a6f4c9a22ef39b7f23b5701ab5130216b340ba2c
cbasil <cbasil@126.com>
2024-03-12 17:57:39 +0800

    运行端口配置

--- routers/router.go
+++ routers/router.go
@@ -1,13 +1,17 @@
 package routers
 
-import "github.com/gin-gonic/gin"
+import (
+	"h5/config"
+
+	"github.com/gin-gonic/gin"
+)
 
 var router = gin.Default()
 
 // Run will start the server
 func Run() {
 	getRoutes()
-	router.Run(":8787")
+	router.Run(config.GlobalConfig.GetString("app.port"))
 }
 
 // getRoutes will create our routes of our entire application
