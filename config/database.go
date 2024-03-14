05060f8f20da46cbec07119272c624779ecb54ef
cbasil <cbasil@126.com>
2024-03-14 11:43:07 +0800

    gorm日志

--- config/database.go
+++ config/database.go
@@ -28,8 +28,12 @@ func GetDbDatabase(database string) (*gorm.DB, error) {
 	}
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", conf.Mysql.User, conf.Mysql.Password, conf.Mysql.Host, conf.Mysql.Port, database)
 	// 初始化GORM日志配置
+	f, err := os.OpenFile(`./gorm.log`, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("logger.Setup err: %v", err)
+	}
 	newLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		log.New(f, "\r\n", log.LstdFlags), // io writer
 		logger.Config{
 			SlowThreshold:             time.Second, // Slow SQL threshold
 			LogLevel:                  logger.Info, // Log level(这里记得根据需求改一下)
