a00bd523d172c508d676ba74ffb5f213de60a4e0
cbasil <cbasil@126.com>
2024-03-15 11:00:58 +0800

    路由调整

--- controllers/index.go
+++ controllers/index.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"ally/config"
 	"ally/utils"
-	"ally/utils/logging"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -14,69 +13,6 @@ import (
 
 func Index(c *gin.Context) {
 	c.String(200, "测试页面")
-}
-
-func Xinhua(c *gin.Context) {
-	at := c.Query("at")
-	if at != "sfdjwie2ji239324" {
-		logging.Error("非法访问")
-		c.String(200, "非法访问")
-		return
-	}
-
-	db, err := config.GetDb()
-
-	if err != nil {
-		c.String(http.StatusOK, err.Error())
-	}
-
-	type Result struct {
-		Code      string `json:"code" tag:"编码"`
-		Status    string `json:"status" tag:"状态"`
-		Remark    string `json:"remark" tag:"标记"`
-		Mobile    string `json:"mobile" tag:"手机号"`
-		Contact   string `json:"contact" tag:"联系人"`
-		Organ     string `json:"organ" tag:"机构"`
-		Work_num  string `json:"work_num" tag:"工号"`
-		Order_no  string `json:"order_no" tag:"订单号"`
-		Contact1  string `json:"contact1" tag:"收货人"`
-		Mobile1   string `json:"mobile1" tag:"收货手机"`
-		Province  string `json:"province" tag:"省"`
-		City      string `json:"city" tag:"市"`
-		Area      string `json:"area" tag:"区"`
-		Address   string `json:"address" tag:"地址"`
-		Ship_name string `json:"ship_name" tag:"快递公司"`
-		Ship_no   string `json:"ship_no" tag:"快递单号"`
-		C_time    string `json:"c_time" tag:"创建时间"`
-	}
-
-	var result []Result
-
-	sqlQuery := "select a.code,a.status,a.remark,c.work_num,c.mobile,c.contact,c.organ,d.order_no,d.contact as contact1,d.mobile as mobile1,d.province,d.city,d.area,d.address,d.ship_name,d.ship_no,d.c_time from car_coupon a left join car_member b on a.user_id = b.id LEFT JOIN car_order_photo_agent c  on b.mobile = c.mobile and c.company = 19 LEFT JOIN car_order_photo d on a.id = d.coupon_id and d.status != -1 where a.batch_num = 'P2401291323' and a.status !=0"
-
-	db.Raw(sqlQuery).Find(&result)
-	for k, v := range result {
-		status := "已激活"
-		remark := "未分享"
-		num, _ := strconv.Atoi(v.Status)
-		if v.Remark == "1" {
-			remark = "已分享"
-		} else if v.Remark != "" && v.Remark != "1" {
-			remark = "已领取"
-		}
-		if num == 2 {
-			status = "已下单"
-			remark = "已下单"
-		}
-		result[k].Status = status
-		result[k].Remark = remark
-		if v.C_time != "" {
-			result[k].C_time = utils.FormatDateByString(v.C_time)
-		}
-	}
-
-	utils.Down(result, "新华保险摆台", c)
-
 }
 
 func Hngx(c *gin.Context) {
