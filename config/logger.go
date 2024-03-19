3c7d5b888de0c69820145de5217666f7f0cc98b9
cbasil <cbasil@126.com>
2024-03-19 09:05:28 +0800

    json日志格式

--- config/logger.go
+++ config/logger.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"io"
 	"log/slog"
 	"os"
 	"path/filepath"
@@ -23,7 +24,7 @@ func SetupSlog() {
 		slog.Error("配置文件解析失败", err)
 	}
 	// 初始化日志
-	log := &lumberjack.Logger{
+	logFile := &lumberjack.Logger{
 		Filename:   conf.Logger.Filename,   // 日志文件的位置
 		MaxSize:    conf.Logger.MaxSize,    // 文件最大尺寸（以MB为单位）
 		MaxBackups: conf.Logger.MaxBackups, // 保留的最大旧文件数量
@@ -31,33 +32,21 @@ func SetupSlog() {
 		Compress:   true,                   // 是否压缩/归档旧文件
 		LocalTime:  true,                   // 使用本地时间创建时间戳
 	}
-
-	opts := &slog.HandlerOptions{
-		Level: slog.LevelDebug,
-		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
-			if a.Key == slog.LevelKey {
-				level := a.Value.Any().(slog.Level)
-				_, file, line, ok := runtime.Caller(DefaultCallerDepth)
-				if ok {
-					logPrefix = fmt.Sprintf("[%s][%s:%d]", level.String(), filepath.Base(file), line)
-				} else {
-					logPrefix = fmt.Sprintf("[%s]", level.String())
-				}
-
-				a.Value = slog.StringValue(logPrefix)
-			}
-
-			return a
-		},
+	logOpts := slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelDebug,
 	}
+
 	env := os.Getenv("GIN_MODE")
 	if env == "release" {
-		opts.Level = slog.LevelError
+		logOpts.Level = slog.LevelError
 	}
 
-	logger := slog.New(slog.NewTextHandler(log, opts))
+	logger := slog.New(slog.NewJSONHandler(io.MultiWriter(os.Stdout, logFile), &logOpts))
 
 	slog.SetDefault(logger)
+	logger.Info("初始化日志完成")
+
 }
 
 func setPrefix() string {
