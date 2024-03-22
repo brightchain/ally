9871b5a02a37da420f37f17465b148b8a79b7e5a
cbasil <cbasil@126.com>
2024-03-22 11:34:00 +0800

    自定义日志级别

--- config/logger.go
+++ config/logger.go
@@ -9,28 +9,37 @@ import (
 )
 
 func SetupSlog() {
-	var conf AppConfig
-	err := GlobalConfig.Unmarshal(&conf)
-	if err != nil {
-		slog.Error("配置文件解析失败", err)
-	}
+	logConf := GlobalConfig.Sub("logger")
+	filename := logConf.GetString("filename")
+	maxSize := logConf.GetInt("maxSize")
+	maxBackups := logConf.GetInt("maxBackups")
+	maxAge := logConf.GetInt("maxAge")
+	level := logConf.GetString("level")
 	// 初始化日志
 	logFile := &lumberjack.Logger{
-		Filename:   conf.Logger.Filename,   // 日志文件的位置
-		MaxSize:    conf.Logger.MaxSize,    // 文件最大尺寸（以MB为单位）
-		MaxBackups: conf.Logger.MaxBackups, // 保留的最大旧文件数量
-		MaxAge:     conf.Logger.MaxAge,     // 保留旧文件的最大天数
-		Compress:   true,                   // 是否压缩/归档旧文件
-		LocalTime:  true,                   // 使用本地时间创建时间戳
+		Filename:   filename,   // 日志文件的位置
+		MaxSize:    maxSize,    // 文件最大尺寸（以MB为单位）
+		MaxBackups: maxBackups, // 保留的最大旧文件数量
+		MaxAge:     maxAge,     // 保留旧文件的最大天数
+		Compress:   true,       // 是否压缩/归档旧文件
+		LocalTime:  true,       // 使用本地时间创建时间戳
 	}
 	logOpts := slog.HandlerOptions{
 		AddSource: true,
 		Level:     slog.LevelDebug,
 	}
 
-	env := os.Getenv("GIN_MODE")
-	if env == "release" {
+	switch level {
+	case "debug":
+		logOpts.Level = slog.LevelDebug
+	case "info":
+		logOpts.Level = slog.LevelInfo
+	case "error":
 		logOpts.Level = slog.LevelError
+	case "warn":
+		logOpts.Level = slog.LevelWarn
+	default:
+		logOpts.Level = slog.LevelDebug
 	}
 
 	logger := slog.New(slog.NewJSONHandler(io.MultiWriter(os.Stdout, logFile), &logOpts))
