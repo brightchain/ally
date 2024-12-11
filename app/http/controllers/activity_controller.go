68794e0f7c14d243c1082b793e437ec6c9a2817f
cbasil <cbasil@126.com>
2024-12-11 22:13:06 +0800

    取消订单开发

--- app/http/controllers/activity_controller.go
+++ app/http/controllers/activity_controller.go
@@ -32,6 +32,7 @@ func (a *CsYdUser) TableName() string {
 }
 type TmpYd struct {
 	Mobile string `gorm:"column:mobile"`
+	OrderNo string `gorm:"column:order_no"`
 }
 
 func (a *TmpYd) TableName() string {
@@ -63,6 +64,8 @@ type ExportOrder struct {
 	PrizeID         int    `gorm:"column:prize_id"`
 	CouponID        int    `gorm:"column:coupon_id"`
 	LotteryTime     string `gorm:"column:lottery_time"`
+	OrderNo         string `gorm:"column:order_no"`
+	OrderTime       string `gorm:"column:order_time"`
 }
 
 func (a *Activity) UserReset(c *gin.Context) {
@@ -211,7 +214,7 @@ func exportToExcel(orders []ExportOrder) error {
 
 	// 设置表头
 	headers := []string{
-		"代理人姓名", "代理人手机号", "工号", "机构编码", "机构名称",
+		"代理人姓名", "代理人手机号", "工号",  "机构名称",
 		"客户姓名", "手机号", "客户地址", "奖品名称",
 		"奖品ID", "权益ID", "抽奖时间",
 	}
@@ -224,6 +227,148 @@ func exportToExcel(orders []ExportOrder) error {
 	for row, order := range orders {
 		data := []interface{}{
 			order.AgentName, order.AgentMobile, order.AgentCode,
+			 order.BranchName,
+			order.CustomerName, order.CustomerMobile, order.CustomerAddress,
+			order.OrderNo, order.OrderTime,
+		}
+		for col, value := range data {
+			cell, _ := excelize.CoordinatesToCellName(col+1, row+2)
+			f.SetCellValue(sheetName, cell, value)
+		}
+	}
+
+	// 保存文件
+	filename := fmt.Sprintf("英大订单取消%s.xlsx", time.Now().Format("20060102"))
+	return f.SaveAs(filename)
+}
+
+func (a *Activity) CancelOrder(c *gin.Context){
+	// 初始化数据库连接
+	db := model.RDB["db1"].Db
+	// 开启事务
+	tx := db.Begin()
+	if tx.Error != nil {
+		c.String(http.StatusOK, "开启事务失败")
+		return 
+	}
+
+	// 查询订单数据
+	orders, err := a.getCanelOrders(tx)
+	if err != nil {
+		c.String(http.StatusOK, fmt.Sprintf("获取订单失败: %v", err))
+		return
+	}
+
+	// 如果没有订单，直接返回
+	if len(orders) == 0 {
+		c.String(http.StatusOK, "没有找到符合条件的订单")	
+		return
+	}
+
+	// 导出Excel
+	err = exportOrderToExcel(orders)
+	if err != nil {
+		tx.Rollback()
+		c.String(http.StatusOK, fmt.Sprintf("导出Excel失败: %v", err))
+		return
+	}
+
+	var couponIDs []int
+	for _, order := range orders {
+		couponIDs = append(couponIDs, order.CouponID)
+	}
+
+	now := time.Now()
+
+	// 批量更新
+	err = tx.Model(&CsYdUser{}).
+		Where("coupon_id IN ?", couponIDs).
+		Updates(map[string]interface{}{
+			"order_no":   "",
+			"order_time" :  0,
+			"u_time": now.Unix(),
+		}).Error
+
+	if err != nil {
+		tx.Rollback()
+		c.String(http.StatusOK, fmt.Sprintf("更新订单失败: %v", err))
+		return
+	}
+
+	// 更新优惠券状态
+	err = tx.Model(&CarCoupon{}).
+		Where("id IN ?", couponIDs).
+		Updates(map[string]interface{}{
+			"status":    1,
+		}).Error
+
+	if err != nil {
+		tx.Rollback()
+		c.String(http.StatusOK, fmt.Sprintf("更新优惠券状态失败: %v", err))
+		return
+	}
+
+	// 提交事务
+	tx.Commit()
+	c.String(http.StatusOK, "订单取消成功")
+	
+}
+
+// 获取订单数据
+func (a *Activity) getCanelOrders(tx *gorm.DB) ([]ExportOrder, error) {
+	var exportOrders []ExportOrder
+	// 首先获取tmp_yd表中的手机号
+	var mobileList []TmpYd
+	err := tx.Model(&TmpYd{}).Scan(&mobileList).Error
+	if err != nil || len(mobileList) == 0 {
+		return nil, fmt.Errorf("未找到有效的手机号列表: %v", err)
+	}
+	var orders []string
+	for _, v := range mobileList {
+		orders = append(orders, v.OrderNo)
+	}
+	fmt.Println(orders)
+    sql := `select 
+				b.name,
+				b.mobile,
+				b.code,
+				b.agent_name branch_name,
+				a.contact customer_name,
+				a.mobile customer_mobile,
+				a.address customer_address,
+				a.order_no,
+				FROM_UNIXTIME(order_time) order_time
+				from cs_yd_user a LEFT JOIN cs_yd_custom_v b on a.activity=25 and a.agt_work_num = b.code and b.activity =25 
+				where a.order_no in (?) 
+				`
+	// 复杂的连表查询
+	err1 := tx.Raw(sql, orders).Scan(&exportOrders).Error
+
+	return exportOrders, err1
+}
+
+func exportOrderToExcel(orders []ExportOrder) error {
+	// 之前的Excel导出代码保持不变
+	f := excelize.NewFile()
+	sheetName := "数据明细"
+	index, _ := f.NewSheet(sheetName)
+	f.SetActiveSheet(index)
+
+	// 设置表头
+	headers := []string{
+		"代理人", "代理人手机号", "工号", "机构名称",
+		"客户姓名", "客户手机号", "客户地址", "订单编号",
+		"下单时间",
+	}
+	for col, header := range headers {
+		cell, _ := excelize.CoordinatesToCellName(col+1, 1)
+		f.SetCellValue(sheetName, cell, header)
+	}
+
+	// 写入数据
+	for row, order := range orders {
+		data := []interface{}{
+			order.AgentName, order.AgentMobile,
 			order.AgentOrgCode, order.BranchName,
 			order.CustomerName, order.CustomerMobile, order.CustomerAddress,
 			order.PrizeName, order.PrizeID, order.CouponID,
@@ -239,3 +384,4 @@ func exportToExcel(orders []ExportOrder) error {
 	filename := fmt.Sprintf("order_export_%s.xlsx", time.Now().Format("20060102_150405"))
 	return f.SaveAs(filename)
 }
+
